f3d1ec25333de7ab0d97606c780f180b6373bb1b
Nyaikamba <dmbzb@mail.umkc.edu>
2018-03-04 23:08:59 -0600

    Login Test Update

--- app/src/main/java/e/android/dramaalert/MainActivity.java
+++ app/src/main/java/e/android/dramaalert/MainActivity.java
@@ -1,13 +1,45 @@
 package e.android.dramaalert;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText Username;
+    private EditText Password;
+    private Button Login;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Username = findViewById(R.id.etUsername);
+        Password = findViewById(R.id.etPassword);
+        Login = findViewById(R.id.btnLogin);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Username.getText().toString(), Password.getText().toString());
+
+            }
+        });
+
+    }
+
+    private void validate(String UserName, String UserPassword)
+    {
+        if ((UserPassword.equals("12345")) && (UserName.equals("Administrator"))); {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+
     }
 }
